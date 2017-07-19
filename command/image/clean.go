a6c68f3f68f3265fe1f42e518a10fdc9143795d1
Wei Ji <wei.ji@anz.com>
2017-07-19 17:42:41 +1000

    update clean image command

--- command/image/clean.go
+++ command/image/clean.go
@@ -7,44 +7,103 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/stararks/dct/command/container"
-
 	"github.com/spf13/cobra"
 )
 
 func CmdcleanImageCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "cleani",
-		Short: "Clean dangling images",
+		Use:   "cleani [reserve-file]",
+		Short: "delete images",
 		Run: func(cmd *cobra.Command, args []string) {
-			cleanDangling()
+			if len(args) == 1 {
+				images := getCacheImages([]string{"images", "-q"})
+				if images == nil {
+					return
+				}
+				reserve, err := getReserveImageFromFile(args[0])
+				if err != nil {
+					fmt.Println("Get reserved images error")
+					return
+				}
+				if reserve == nil {
+					cleanDangling()
+				} else {
+					for _, r := range reserve {
+						for i, v := range images {
+							if v == r {
+								images = append(images[:i], images[i+1:]...)
+							}
+						}
+					}
+					cleanImage(images)
+				}
+			} else {
+				cleanDangling()
+			}
+			return
 		},
 	}
 	return cmd
 }
 
+func cleanImage(images []string) {
+	for _, image := range images {
+		_, err := exec.Command("docker", "rmi", "--force", image).Output()
+		if err != nil {
+			fmt.Printf("Image (ID %s) can't be deleted.\n", image)
+			// fmt.Printf("Enter [y] to clean containers default[N/No]:")
+			// input := bufio.NewScanner(os.Stdin)
+			// if input.Scan() && input.Text() == "y" {
+			// 	container.CleanContainers()
+			// }
+		}
+	}
+
+}
+
 func cleanDangling() {
-	args := []string{"images", "-q", "--filter", "dangling=true"}
+	images := getCacheImages([]string{"images", "-q", "--filter", "dangling=true"})
+	if images != nil {
+		fmt.Println("Only Dangling image will be removed from local cache")
+		cleanImage(images)
+	}
+}
+
+func getReserveImageFromFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var items []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		items = append(items, scanner.Text())
+	}
+	return items, scanner.Err()
+}
+
+func getCacheImages(args []string) []string {
 	out, err := exec.Command("docker", args...).Output()
 	if err != nil {
-		fmt.Println("Get dangling images error")
-		return
+		fmt.Println("Get images error")
+		return nil
 	}
 	images := strings.Fields(strings.TrimSpace(strings.Replace(string(out), "\n", " ", -1)))
 
 	if len(images) == 0 {
-		return
+		return nil
 	}
 
-	ids := []string{"rmi"}
-	ids = append(ids, images...)
-	out, err = exec.Command("docker", ids...).Output()
-	if err != nil {
-		fmt.Println("Dangling images can't be cleaned. It is possible container running base on the images. Running docker native command to stop and clean the corresponding containers or use dtool cleanc subcommand to stop and clean ALL containers")
-		fmt.Printf("Enter [y] to run cleancontainers default[No]:")
-		input := bufio.NewScanner(os.Stdin)
-		if input.Scan() && input.Text() == "y" {
-			container.CleanContainers()
+	encountered := map[string]bool{}
+	result := []string{}
+	for v := range images {
+		if encountered[images[v]] != true {
+			encountered[images[v]] = true
+			result = append(result, images[v])
 		}
 	}
+
+	return result
 }
